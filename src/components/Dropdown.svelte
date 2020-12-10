07ace1b1802b11da6348d0509a4359c28f2484b5
Nikoloz Razmadze <raloz1@gmail.com>
2020-12-11 03:43:18 +0400

    Move dropdown dependency in Dropdown.svelte

--- src/components/Dropdown.svelte
+++ src/components/Dropdown.svelte
@@ -1,27 +1,27 @@
 <script>
+  import dropdown from "bootstrap/js/dist/dropdown";
 
-export let keys;
-export let value;
-
+  export let keys;
+  export let value;
 </script>
 
 <button
-    class="btn btn-outline-light dropdown-toggle"
-    type="button"
-    data-toggle="dropdown"
-    aria-haspopup="true"
-    aria-expanded="false">
-    { value }
+  class="btn btn-outline-light dropdown-toggle"
+  type="button"
+  data-toggle="dropdown"
+  aria-haspopup="true"
+  aria-expanded="false">
+  {value}
 </button>
 <div class="dropdown-menu">
-    {#each keys as k}
-        <div>
-            <a
-                class="dropdown-item"
-                href={'#'}
-                on:click|preventDefault={() => value = k}>
-                { k }
-            </a>
-        </div>
-    {/each}
+  {#each keys as k}
+    <div>
+      <a
+        class="dropdown-item"
+        href={'#'}
+        on:click|preventDefault={() => (value = k)}>
+        {k}
+      </a>
+    </div>
+  {/each}
 </div>
--- src/components/SearchForm.svelte
+++ src/components/SearchForm.svelte
@@ -1,60 +1,56 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+  import { navigate } from "svelte-routing";
+  import regions from "../../data/regions.json";
+  import "../scss/search-form.scss";
+  import Dropdown from "./Dropdown.svelte";
 
-import { createEventDispatcher } from 'svelte';
-import dropdown from 'bootstrap/js/dist/dropdown';
-import { navigate } from 'svelte-routing';
-import regions from '../../data/regions.json';
-import '../scss/search-form.scss';
-import Dropdown from './Dropdown.svelte';
+  export let loading;
 
-export let loading;
+  const REGION_KEY = "region";
+  const USERNAME_KEY = "username";
 
-const REGION_KEY = 'region';
-const USERNAME_KEY = 'username';
+  const regionKeys = Object.keys(regions);
 
-const regionKeys = Object.keys(regions);
+  const path = location.pathname.split("/").slice(1);
+  let region;
+  let username;
 
-const path = location.pathname.split('/').slice(1);
-let region;
-let username;
-
-if (2 === path.length && regionKeys.includes(path[0])) {
-    [ region, username ] = path;
-} else {
+  if (2 === path.length && regionKeys.includes(path[0])) {
+    [region, username] = path;
+  } else {
     region = localStorage.getItem(REGION_KEY) || regionKeys[0];
-    username = localStorage.getItem(USERNAME_KEY) || '';
-}
+    username = localStorage.getItem(USERNAME_KEY) || "";
+  }
 
-const dispatch = createEventDispatcher();
+  const dispatch = createEventDispatcher();
 
-function onSubmit() {
-
+  function onSubmit() {
     localStorage.setItem(REGION_KEY, region);
     localStorage.setItem(USERNAME_KEY, username);
     navigate(`/${region}/${username}`);
-    dispatch('update', { username, region });
-}
-
+    dispatch("update", { username, region });
+  }
 </script>
 
 <form on:submit|preventDefault={onSubmit}>
-    <div class="form-group">
-        <div class="input-group">
-            <input
-                type="text"
-                class="form-control form-control-lg"
-                bind:value={ username }
-                placeholder="Summoner name"
-                aria-label="Summoner name"
-                required>
-            <div class="input-group-append">
-                <Dropdown keys={ regionKeys } bind:value={ region } />
-            </div>
-            <div class="input-group-append">
-                <button class="btn btn-outline-light wide" disabled={ loading }>
-                    { loading ? 'Loading...' : 'Search' }
-                </button>
-            </div>
-        </div>
+  <div class="form-group">
+    <div class="input-group">
+      <input
+        type="text"
+        class="form-control form-control-lg"
+        bind:value={username}
+        placeholder="Summoner name"
+        aria-label="Summoner name"
+        required />
+      <div class="input-group-append">
+        <Dropdown keys={regionKeys} bind:value={region} />
+      </div>
+      <div class="input-group-append">
+        <button class="btn btn-outline-light wide" disabled={loading}>
+          {loading ? 'Loading...' : 'Search'}
+        </button>
+      </div>
     </div>
+  </div>
 </form>
