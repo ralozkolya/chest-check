992c01a47e8d624637eb15d56f7c65c21cc9e83f
Nikoloz Razmadze <raloz1@gmail.com>
2022-02-26 04:41:44 +0400

    Remove padding from collapse

--- src/components/Champion.svelte
+++ src/components/Champion.svelte
@@ -1,24 +1,26 @@
 <script>
-  import { onMount } from 'svelte';
-  import Collapse from 'bootstrap/js/dist/collapse';
-  import Bluebird from 'bluebird';
+  import { onMount } from "svelte";
+  import Collapse from "bootstrap/js/dist/collapse";
+  import Bluebird from "bluebird";
 
-  import { forgottenIds } from '../stores/champions';
+  import { forgottenIds } from "../stores/champions";
 
-  import '../scss/champion.scss';
+  import "../scss/champion.scss";
 
   export let champion;
   export let parent;
 
-  const transform = name => name.replace(/\W/g, '').toLowerCase();
+  const transform = (name) => name.replace(/\W/g, "").toLowerCase();
 
   let collapseRef, collapse, show;
 
   const toggleDetails = async () => {
     if (parent) {
-      const collapses = document.querySelector(parent).querySelectorAll('.collapse');
-      await Bluebird.each(collapses, async ref => {
-        if (ref.classList.contains('show')) {
+      const collapses = document
+        .querySelector(parent)
+        .querySelectorAll(".collapse");
+      await Bluebird.each(collapses, async (ref) => {
+        if (ref.classList.contains("show")) {
           Collapse.getOrCreateInstance(ref).hide();
           await Bluebird.delay(350);
         }
@@ -28,30 +30,29 @@
     collapse.toggle();
   };
 
-  const onForget = id => {
+  const onForget = (id) => {
     if ($forgottenIds.has(id)) {
-      $forgottenIds.delete(id)
+      $forgottenIds.delete(id);
     } else {
       $forgottenIds.add(id);
     }
     forgottenIds.set($forgottenIds);
   };
 
-  let forgotten
+  let forgotten;
   $: forgotten = $forgottenIds.has(champion.id);
 
   onMount(async () => {
     collapse = Collapse.getOrCreateInstance(collapseRef, { toggle: false });
-    collapseRef.addEventListener('show.bs.collapse', () => show = true);
-    collapseRef.addEventListener('hide.bs.collapse', () => show = false);
+    collapseRef.addEventListener("show.bs.collapse", () => (show = true));
+    collapseRef.addEventListener("hide.bs.collapse", () => (show = false));
   });
-
 </script>
 
-<div class="wrapper" class:open={ show }>
+<div class="wrapper" class:open={show}>
   <div class="champion">
     <div class="img-container">
-      <a href="/" on:click|preventDefault={ toggleDetails }>
+      <a href="/" on:click|preventDefault={toggleDetails}>
         <img class="w-100" src={champion.image} alt={champion.name} />
       </a>
     </div>
@@ -59,21 +60,26 @@
   <div
     bind:this={collapseRef}
     class="collapse details"
-    style="--background-image: url('{ champion.splash }')">
-    <h5 class="mb-3">{ champion.name }</h5>
-    <a
-      target="_blank"
-      rel="noreferrer"
-      href={`https://u.gg/lol/champions/${transform(champion.name)}/build`}
-      class="btn btn-sm btn-primary">
-      Visit U.GG page
-    </a>
-    <button
-      class="btn btn-sm"
-      class:btn-danger={ !forgotten }
-      class:btn-success={ forgotten }
-      on:click={ () => onForget(champion.id) }>
-      { forgotten ? 'Restore' : 'Forget' }
-    </button>
+    style="--background-image: url('{champion.splash}')"
+  >
+    <div class="p-3">
+      <h5 class="mb-3">{champion.name}</h5>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href={`https://u.gg/lol/champions/${transform(champion.name)}/build`}
+        class="btn btn-sm btn-primary"
+      >
+        Visit U.GG page
+      </a>
+      <button
+        class="btn btn-sm"
+        class:btn-danger={!forgotten}
+        class:btn-success={forgotten}
+        on:click={() => onForget(champion.id)}
+      >
+        {forgotten ? "Restore" : "Forget"}
+      </button>
+    </div>
   </div>
 </div>
