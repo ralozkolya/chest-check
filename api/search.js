6903fe3a97de54fb7e96f13017db5c9bc59e531d
Nikoloz Razmadze <raloz1@gmail.com>
2022-11-16 04:25:58 +0400

    Update version

--- api/search.js
+++ api/search.js
@@ -1,17 +1,16 @@
-import axios from 'axios';
-import assert from 'assert';
-import regions from '../data/regions.json';
+import axios from "axios";
+import assert from "assert";
+import regions from "../data/regions.json";
 
 // To retrieve the versions list, check
 // https://ddragon.leagueoflegends.com/api/versions.json
 // Champion list
-// https://ddragon.leagueoflegends.com/cdn/12.20.1/data/en_US/championFull.json
-import champMap from '../data/championFull.json';
+// https://ddragon.leagueoflegends.com/cdn/12.22.1/data/en_US/championFull.json
+import champMap from "../data/championFull.json";
 
 export default async (req, res) => {
-
   const { RIOT_KEY } = process.env;
-  const params = { headers: { 'X-Riot-Token': RIOT_KEY } };
+  const params = { headers: { "X-Riot-Token": RIOT_KEY } };
 
   try {
     validate(req.query);
@@ -19,44 +18,51 @@ export default async (req, res) => {
     return res.status(422).send(e.message);
   }
 
-
   const { username, region } = req.query;
   const url = `https://${region}.api.riotgames.com`;
 
   try {
+    const {
+      data: { id },
+    } = await axios.get(
+      `${url}/lol/summoner/v4/summoners/by-name/${username}`,
+      params
+    );
 
-    const { data: { id } } = await axios.get(`${url}/lol/summoner/v4/summoners/by-name/${username}`, params);
+    const mastery = await axios.get(
+      `${url}/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}`,
+      params
+    );
 
-    const mastery = await axios.get(`${url}/lol/champion-mastery/v4/champion-masteries/by-summoner/${id}`, params);
-
-    const mapped = mastery.data.map(champ => {
-
+    const mapped = mastery.data.map((champ) => {
       const full = champMap.data[champMap.keys[champ.championId]];
 
-      return full && {
-        id: champ.championId,
-        points: champ.championPoints,
-        name: full.name,
-        level: champ.championLevel,
-        chestGranted: champ.chestGranted,
-        image: `https://ddragon.leagueoflegends.com/cdn/${champMap.version}/img/champion/${full.image.full}`,
-        splash: `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${full.id}_0.jpg`,
-      };
+      return (
+        full && {
+          id: champ.championId,
+          points: champ.championPoints,
+          name: full.name,
+          level: champ.championLevel,
+          chestGranted: champ.chestGranted,
+          image: `https://ddragon.leagueoflegends.com/cdn/${champMap.version}/img/champion/${full.image.full}`,
+          splash: `https://ddragon.leagueoflegends.com/cdn/img/champion/splash/${full.id}_0.jpg`,
+        }
+      );
     });
 
-    res.send(mapped.filter(champ => champ));
-
+    res.send(mapped.filter((champ) => champ));
   } catch (e) {
-    res.status(e.response && e.response.status || 500).send(e);
+    res.status((e.response && e.response.status) || 500).send(e);
   }
-
 };
-
 
 function validate(data) {
   const { username, region } = data;
 
   const regionValues = Object.values(regions);
-  assert(regionValues.includes(region), `'region' must be one of ${regionValues.join(', ')}`);
+  assert(
+    regionValues.includes(region),
+    `'region' must be one of ${regionValues.join(", ")}`
+  );
   assert(username, "'username' is mandatory");
 }
